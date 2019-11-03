40332781afc55a26d957be821adbc0d9a307242e
jingtu87 <tujing87@gmail>
2019-11-03 14:34:26 -0600

    Complete Lesson 20

---
+++ lessons/20-js-conversions/index.js
@@ -0,0 +1,17 @@
+let age = 32;
+let firstName = "Jing";
+let completeStatus = false;
+let finalScore = null;
+let winningNumbers;
+
+console.log("Age:", age);
+console.log("First name:", firstName);
+console.log("Complete status:", completeStatus);
+console.log("Final score:", finalScore);
+console.log("Winning numbers:", winningNumbers);
+
+console.log("Age:", String(age));
+console.log("First name:", Boolean(firstName));
+console.log("Complete status:", Number(completeStatus));
+console.log("Final score:", Boolean(finalScore));
+console.log("Winning numbers:", String(winningNumbers));
